dc0d9d92a431831080aee7b2a7da97c66fc786de
Holiverse <hpyang1996@163.com>
2026-04-12 21:43:17 +0800

    update

--- CAT_statement/S_0007.lean
+++ CAT_statement/S_0007.lean
@@ -5,5 +5,12 @@ open CategoryTheory
 variable {C D : Type*} [Category C] [Category D]
 
 theorem karoubi_universal_property [IsIdempotentComplete D] (F : C ⥤ D) :
-    ∃! (F' : (Idempotents.Karoubi C) ⥤ D), (Idempotents.toKaroubi C) ⋙ F' = F := by
+    ∃ (F' : Idempotents.Karoubi C ⥤ D), Nonempty ((Idempotents.toKaroubi C) ⋙ F' ≅ F) := by
+  sorry
+
+theorem karoubi_universal_property_unique_up_to_iso [IsIdempotentComplete D] {F : C ⥤ D}
+    {F₁ F₂ : Idempotents.Karoubi C ⥤ D}
+    (h₁ : Nonempty ((Idempotents.toKaroubi C) ⋙ F₁ ≅ F))
+    (h₂ : Nonempty ((Idempotents.toKaroubi C) ⋙ F₂ ≅ F)) :
+    Nonempty (F₁ ≅ F₂) := by
   sorry
